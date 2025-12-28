7affd98e9f8a439d45fba09df90d55e4d62b77ec
bhavana1312 <bhavana13sree@gmail.com>
2025-12-28 21:43:13 +0530

    test

--- src/main/java/com/utilitybilling/authservice/config/SecurityConfig.java
+++ src/main/java/com/utilitybilling/authservice/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+
+@SuppressWarnings("java:S4502")
 @Configuration
 public class SecurityConfig {
 
